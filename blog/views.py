f939d8bffda815bb35bb394b1ca8926faf0ff687
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-04-22 05:53:12 +0200

    post_detail and forms.py added

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def frontpage(request):
     posts = Post.objects.all()
     return render(request, "blog/frontpage.html", {"posts": posts})
+
+def post_detail(request, slug):
+    # if slug from the button is same as the slug in the database...
+    post = Post.objects.get(slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post})
